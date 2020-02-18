850864ca80bc27e610945855370260000c0c8752
T-Rekt <t.to.the.rekt@gmail.com>
2020-02-18 16:01:26 +0700

    Updated stats and testStat.py

--- testStat.py
+++ testStat.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import os
 
 problems = open('author.txt').read().strip().split('\n')
 
@@ -14,21 +15,24 @@ stats = []
 
 for author in authorDict.keys():
     missing = []
+    empty = []
 
     owned = authorDict[author]
 
     for problemName in owned:
         if not (problemName in haveTests):
             missing.append(problemName)
+        elif not len(os.listdir('./tests/' + problemName)):
+            empty.append(problemName)
 
-    stats.append([author, owned, missing])
+    stats.append([author, owned, missing, empty])
 
-stats.sort(key=lambda item: len(item[2]), reverse=True)
+stats.sort(key=lambda item: len(item[2]) + len(item[3]), reverse=True)
 
 s = ''
 
 for item in stats:
-    [author, owned, missing] = item
-    s += author + ',' + str(len(owned)) + ',' + str(len(missing)) + ',' + '|'.join(missing) + '\n'
+    [author, owned, missing, empty] = item
+    s += author + ',' + str(len(owned)) + ',' + str(len(missing)) + ',' + str(len(empty)) + ',' + '|'.join(missing) + ',' + '|'.join(empty) + '\n'
 
-open('stats.csv', 'w').write('Author,Problems owned, Problems with tests missing (count), Problems with tests missing\n' + s)
+open('stats.csv', 'w').write('Author,Problems owned, Problems with tests missing (count),Problems with 0 test (count),Problems with tests missing, Problems with 0 test\n' + s)
